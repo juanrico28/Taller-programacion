3f6093b0c4ff00ca0717f128dc823f805d6e572f
juanrico28 <100099534+juanrico28@users.noreply.github.com>
2022-02-20 18:31:38 -0500

    Add files via upload

---
+++ Taller1/Taller1/src/main/java/umb/taller1/primerServlet.java
@@ -0,0 +1,66 @@
+package umb.taller1;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Juan Rico
+ */
+public class primerServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String login = request.getParameter("username");
+        String password = request.getParameter("passwd");
+        StringBuffer sb = new StringBuffer();
+        sb.append("<br>login: "+login);
+        sb.append("<br>password: "+password);
+        
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Administraci@oacute;n de Usuario</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Ingreso los siguientes datos:" +sb.toString() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+   
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
